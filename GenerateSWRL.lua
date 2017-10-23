9f1b5e0a1a0a4eb6ba806c54b1cbce013f47070c
bpalkmim <bpalkmim@gmail.com>
2017-10-23 13:37:02 -0200

    Iniciado o backend. Também foi criado um arquivo com as strings constantes utilizadas na AST e as keywords de SQL para LPeg.

--- GenerateSWRL.lua
+++ GenerateSWRL.lua
@@ -1,12 +1,122 @@
 -------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------
 -- Arquivo: GenerateSWRL.lua
--- Autor: Bernardo Pinto de Alkmim (bpalkmim@gmail.com)
+-- Autor: Bernardo Alkmim (bpalkmim@gmail.com)
 --
 -- Módulo que gera código SWRL partindo de uma AST de SQL.
 -- É necessário ter o pacote lpeg. Recomendo a instalação via Luarocks.
 -------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------
 
+require "ParseSQL"
+require "ConstantsForParsing"
+
 -- Define o módulo.
 GenerateSWRL = {}
+
+-- TODO criar lista de identificadores
+-- TODO lista de operadores
+-- TODO ver quais outras listas deverão ser criadas
+
+-- Tags para a AST.
+local tag = ConstantsForParsing.getTag()
+
+-------------------------------------------------------------------------------------------
+-- Funções locais auxiliares ao módulo
+-------------------------------------------------------------------------------------------
+
+-- Função que percorre a AST preenchendo a string de saída com o código SWRL de acordo.
+local function scanAST(ast)
+	local ret = ""
+
+	assert(type(ast) == "table", "AST não é uma tabela Lua.")
+
+	if ast["tag"] ~= nil then
+
+		if ast["tag"] == tag["where"] then
+			ret = ret..scanAST(ast[1])
+		elseif ast["tag"] == tag["or"] then
+			ret = ret..writeOr(ast)
+		elseif ast["tag"] == tag["and"] then
+			ret = ret..writeAnd(ast)
+		elseif ast["tag"] == tag["comp"] then
+			ret = ret..writeComp(ast)
+		elseif ast["tag"] == tag["mult"] then
+
+		elseif ast["tag"] == tag["add"] then
+
+		elseif ast["tag"] == tag["colId"] then
+
+		elseif ast["tag"] == tag["in"] then
+
+		-- Casos de nós da AST que não estão dentro do WHERE de SQL.
+		else
+			for _, v in ipairs(ast) do
+				ret = ret..scanAST(v)
+			end
+		end
+	end
+
+	return ret
+end
+
+-- Função que cria um arquivo cujo conteúdo é a string passada por parâmetro, indexando de
+-- acordo com o parâmetro passado. O formato do nome é "output"..i..".swrl"
+local function createOutputFile(text, i)
+	local file = assert(io.open("output"..i.."", "w"))
+	file:write(text)
+	file:close()
+end
+
+-------------------------------------------------------------------------------------------
+-- Funções externas do módulo
+-------------------------------------------------------------------------------------------
+
+-- Função principal do módulo que recebe um arquivo de um código escrito em SQL, faz sua
+-- AST e cria um arquivo em SWRL.
+function GenerateSWRL.generateOutput(fileName, index)
+	local ast = ParseSQL.parseInput(fileName)
+	local out = scanAST(ast)
+	createOutputFile(out, index)
+end
+
+--[[
+Conceitos:
+SimpleExpression
+ExpressionObject
+Literal
+ReferencedColumn
+ComponentOf (ComparisonOperator, Literal e ReferencedColumn)
+
+
+--Predicate hasDescription “l_shipdate <= date ’1998-12-01 ’ - interval ’ 87 days ’”
+--Column hasName “l_shipdate”
+--ComparisonOperator hasDescription “<=”
+
+Conceitos: SimpleExpression, ExpressionObject, Literal, ReferencedColumn e
+o relacionamento ComponentOf da SimpleExpression para todos os ExpressionObject
+(ComparisonOperator, Literal e ReferencedColumn).
+
+Conclusão que deve ser extraída:
+
+SimpleExpression hasDescription “l_shipdate <= date ’1998-12-01 ’ - interval ’ 87 days ’”
+
+ExpressionObject: “l_shipdate”, “<=”, “date ’1998-12-01 ’ - interval ’ 87 days ’”
+(Aqui, pode classificar nas especializações primeiro e depois dizer que também é desse tipo.
+ ComparisonOperator já existe, só precisa relacionar com a SimpleExpression)
+
+Literal hasDescription “date ’1998-12-01 ’ - interval ’ 87 days ’”
+
+ReferencedColumn: Column hasName “l_shipdate” (Aqui é só classificar um objeto existente,
+ coluna que tenha esse nome)
+
+ComparisonOperator hasDescription “<=” ComponentOf
+SimpleExpression hasDescription “l_shipdate <= date ’1998-12-01 ’ - interval ’ 87 days ’”
+
+Literal hasDescription “date ’1998-12-01 ’ - interval ’ 87 days ’”
+ComponentOf SimpleExpression hasDescription “l_shipdate <= date ’1998-12-01 ’ - interval ’ 87 days ’”
+
+ReferencedColumn hasName “l_shipdate” ComponentOf
+SimpleExpression hasDescription “l_shipdate <= date ’1998-12-01 ’ - interval ’ 87 days ’”
+
+]]
--- ParseSQL.lua
+++ ParseSQL.lua
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------
 -- Arquivo: ParseSQL.lua
--- Autor: Bernardo Pinto de Alkmim (bpalkmim@gmail.com)
+-- Autor: Bernardo Alkmim (bpalkmim@gmail.com)
 --
 -- Um módulo Lua para parsear sentenças em SQL, gerando uma AST em forma de tabela.
 -- É necessário ter o pacote lpeg. Recomendo a instalação via Luarocks.
@@ -11,11 +11,12 @@
 -------------------------------------------------------------------------------------------
 
 local lpeg = require "lpeg"
+require "ConstantsForParsing"
 
 -- Define o módulo
 ParseSQL = {}
 
--- TODO 7 parece estoura a pilha por causa de expressões:
+-- TODO 7 parece estourar a pilha por causa de expressões:
 -- dentro de chamada de função um caseClause e expressão interna entre parênteses
 -- não estoura a pilha com parênteses extra na chamada de função
 -- ver como tratar isso
@@ -32,50 +33,7 @@ local tabs = ""
 local errConversion = "Erro de conversão de tabela para string."
 
 -- Tags para a AST.
-local tag = {}
-tag["select"] 		= "SelectionCmd"
-tag["from"] 		= "From"
-tag["join"] 		= "Join"
-tag["where"] 		= "Where"
-tag["id"] 			= "Identifier"
-tag["root"] 		= "Root"
-tag["command"] 		= "Command"
-tag["selectStmt"] 	= "Select"
-tag["columns"] 		= "Columns"
-tag["on"] 			= "On"
-tag["using"] 		= "Using"
-tag["colId"] 		= "Column ID"
-tag["natural"] 		= "Natural"
-tag["inner"] 		= "Inner"
-tag["left"] 		= "Left"
-tag["outer"] 		= "Outer"
-tag["cross"] 		= "Cross"
-tag["and"] 			= "And"
-tag["or"] 			= "Or"
-tag["as"] 			= "As"
-tag["alias"] 		= "Alias"
-tag["orderby"] 		= "Order By"
-tag["groupby"] 		= "Group By"
-tag["date"] 		= "Date"
-tag["interval"] 	= "Interval"
-tag["not"] 			= "Not"
-tag["function"] 	= "Function"
-tag["desc"] 		= "Desc"
-tag["between"] 		= "Between"
-tag["case"] 		= "Case"
-tag["like"]			= "Like"
-tag["escape"]		= "Escape"
-tag["litString"]	= "LitString"
-tag["number"]		= "Number"
-tag["limit"]		= "Limit"
-tag["all"]			= "All"
-tag["offset"]		= "Offset"
-tag["having"]		= "Having"
-tag["in"]			= "In"
-tag["comp"]			= "Comp"
-tag["add"]			= "Add"
-tag["mult"]			= "Mult"
-tag["distinct"]		= "Distinct"
+local tag = ConstantsForParsing.getTag()
 
 -- Elementos Léxicos.
 local space 	= lpeg.S(" \n\t")
@@ -99,63 +57,20 @@ local literalString = lpeg.P("'") * (lpeg.P("\\") * lpeg.P(1) + (1 - (lpeg.S("\\
 
 -- Operadores
 local compOperator 	= lpeg.C(lpeg.P(">="))
-					+ lpeg.C(lpeg.P("<="))
-					+ lpeg.C(lpeg.P("!="))
-					+ lpeg.C(lpeg.P("<>"))
-					+ lpeg.C(lpeg.P(">"))
-					+ lpeg.C(lpeg.P("<"))
-					+ lpeg.C(lpeg.P("="));
+	+ lpeg.C(lpeg.P("<="))
+	+ lpeg.C(lpeg.P("!="))
+	+ lpeg.C(lpeg.P("<>"))
+	+ lpeg.C(lpeg.P(">"))
+	+ lpeg.C(lpeg.P("<"))
+	+ lpeg.C(lpeg.P("="))
 local addOperator 	= lpeg.C(lpeg.P("+"))
-					+ lpeg.C(lpeg.P("-"));
+	+ lpeg.C(lpeg.P("-"))
 local multOperator 	= lpeg.C(lpeg.P("*"))
-					+ lpeg.C(lpeg.P("/"))
-					+ lpeg.C(lpeg.P("%"));
+	+ lpeg.C(lpeg.P("/"))
+	+ lpeg.C(lpeg.P("%"))
 
 -- Palavras-chave.
-local kw = {}
-kw["as"] 		= lpeg.S("Aa") * lpeg.S("Ss")
-kw["in"] 		= lpeg.S("Ii") * lpeg.S("Nn")
-kw["on"] 		= lpeg.S("Oo") * lpeg.S("Nn")
-kw["or"] 		= lpeg.S("Oo") * lpeg.S("Rr")
-kw["all"]		= lpeg.S("Aa") * lpeg.S("Ll") * lpeg.S("Ll")
-kw["and"] 		= lpeg.S("Aa") * lpeg.S("Nn") * lpeg.S("Dd")
-kw["end"] 		= lpeg.S("Ee") * lpeg.S("Nn") * lpeg.S("Dd")
-kw["not"] 		= lpeg.S("Nn") * lpeg.S("Oo") * lpeg.S("Tt")
-kw["from"] 		= lpeg.S("Ff") * lpeg.S("Rr") * lpeg.S("Oo") * lpeg.S("Mm")
-kw["join"] 		= lpeg.S("Jj") * lpeg.S("Oo") * lpeg.S("Ii") * lpeg.S("Nn")
-kw["left"] 		= lpeg.S("Ll") * lpeg.S("Ee") * lpeg.S("Ff") * lpeg.S("Tt")
-kw["full"] 		= lpeg.S("Ff") * lpeg.S("Uu") * lpeg.S("Ll") * lpeg.S("Ll")
-kw["null"] 		= lpeg.S("Nn") * lpeg.S("Uu") * lpeg.S("Ll") * lpeg.S("Ll")
-kw["date"] 		= lpeg.S("Dd") * lpeg.S("Aa") * lpeg.S("Tt") * lpeg.S("Ee")
-kw["true"] 		= lpeg.S("Tt") * lpeg.S("Rr") * lpeg.S("Uu") * lpeg.S("Ee")
-kw["desc"] 		= lpeg.S("Dd") * lpeg.S("Ee") * lpeg.S("Ss") * lpeg.S("Cc")
-kw["case"] 		= lpeg.S("Cc") * lpeg.S("Aa") * lpeg.S("Ss") * lpeg.S("Ee")
-kw["when"] 		= lpeg.S("Ww") * lpeg.S("Hh") * lpeg.S("Ee") * lpeg.S("Nn")
-kw["then"] 		= lpeg.S("Tt") * lpeg.S("Hh") * lpeg.S("Ee") * lpeg.S("Nn")
-kw["else"] 		= lpeg.S("Ee") * lpeg.S("Ll") * lpeg.S("Ss") * lpeg.S("Ee")
-kw["like"]		= lpeg.S("Ll") * lpeg.S("Ii") * lpeg.S("Kk") * lpeg.S("Ee")
-kw["cross"] 	= lpeg.S("Cc") * lpeg.S("Rr") * lpeg.S("Oo") * lpeg.S("Ss") * lpeg.S("Ss")
-kw["limit"]		= lpeg.S("Ll") * lpeg.S("Ii") * lpeg.S("Mm") * lpeg.S("Ii") * lpeg.S("Tt")
-kw["outer"] 	= lpeg.S("Oo") * lpeg.S("Uu") * lpeg.S("Tt") * lpeg.S("Ee") * lpeg.S("Rr")
-kw["inner"] 	= lpeg.S("Ii") * lpeg.S("Nn") * lpeg.S("Nn") * lpeg.S("Ee") * lpeg.S("Rr")
-kw["right"] 	= lpeg.S("Rr") * lpeg.S("Ii") * lpeg.S("Gg") * lpeg.S("Hh") * lpeg.S("Tt")
-kw["using"] 	= lpeg.S("Uu") * lpeg.S("Ss") * lpeg.S("Ii") * lpeg.S("Nn") * lpeg.S("Gg")
-kw["where"] 	= lpeg.S("Ww") * lpeg.S("Hh") * lpeg.S("Ee") * lpeg.S("Rr") * lpeg.S("Ee")
-kw["false"] 	= lpeg.S("Ff") * lpeg.S("Aa") * lpeg.S("Ll") * lpeg.S("Ss") * lpeg.S("Ee")
-kw["escape"]	= lpeg.S("Ee") * lpeg.S("Ss") * lpeg.S("Cc") * lpeg.S("Aa") * lpeg.S("Pp") * lpeg.S("Ee")
-kw["having"]	= lpeg.S("Hh") * lpeg.S("Aa") * lpeg.S("Vv") * lpeg.S("Ii") * lpeg.S("Nn") * lpeg.S("Gg")
-kw["offset"]	= lpeg.S("Oo") * lpeg.S("Ff") * lpeg.S("Ff") * lpeg.S("Ss") * lpeg.S("Ee") * lpeg.S("Tt")
-kw["select"] 	= lpeg.S("Ss") * lpeg.S("Ee") * lpeg.S("Ll") * lpeg.S("Ee") * lpeg.S("Cc") * lpeg.S("Tt")
-kw["natural"] 	= lpeg.S("Nn") * lpeg.S("Aa") * lpeg.S("Tt") * lpeg.S("Uu") * lpeg.S("Rr") * lpeg.S("Aa") * lpeg.S("Ll")
-kw["between"] 	= lpeg.S("Bb") * lpeg.S("Ee") * lpeg.S("Tt") * lpeg.S("Ww") * lpeg.S("Ee") * lpeg.S("Ee") * lpeg.S("Nn")
-kw["orderby"] 	=
-	lpeg.S("Oo") * lpeg.S("Rr") * lpeg.S("Dd") * lpeg.S("Ee") * lpeg.S("Rr") * lpeg.P(" ") * lpeg.S("Bb") * lpeg.S("Yy")
-kw["groupby"] 	=
-	lpeg.S("Gg") * lpeg.S("Rr") * lpeg.S("Oo") * lpeg.S("Uu") * lpeg.S("Pp") * lpeg.P(" ") * lpeg.S("Bb") * lpeg.S("Yy")
-	kw["distinct"]	=
-	lpeg.S("Dd") * lpeg.S("Ii") * lpeg.S("Ss") * lpeg.S("Tt") * lpeg.S("Ii") * lpeg.S("Nn") * lpeg.S("Cc") * lpeg.S("Tt")
-kw["interval"] 	=
-	lpeg.S("Ii") * lpeg.S("Nn") * lpeg.S("Tt") * lpeg.S("Ee") * lpeg.S("Rr") * lpeg.S("Vv") * lpeg.S("Aa") * lpeg.S("Ll")
+local kw = ConstantsForParsing.getKw()
 
 -- Atualização de identifier com as keywords
 local keyWords = lpeg.S("")
